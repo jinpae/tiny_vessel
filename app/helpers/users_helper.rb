f683da0dba267ce063912d8c4943c8c3931e770e
Jin Pae <jin@jinpae.com>
2015-07-11 22:59:26 +0900

    Add header and begin styling forum posts index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 	def profile_image_for(user, *css_class)
-		url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}d=mm"
+		url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?d=mm"
 
 		image_tag url, alt: 'User Profile Image', class: css_class
 	end
