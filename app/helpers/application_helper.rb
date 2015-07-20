07fcaa009a321340a76886f150274ea0e69e83dc
Jin Pae <jin@jinpae.com>
2015-07-20 17:39:24 +0900

    Add page title to each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def pluralize_without_count(count, singular, plural=nil)
 		(count == 1 || count =~ /^1(\.0+)?$/) ? singular : (plural || singular.pluralize)
 	end
+
+	def set_title(title)
+		content_for :title, title.concat(' | Tiny Vessel')
+	end
 end
